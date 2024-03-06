23a670a5745fb01cd50da4660b71c80fbb50eff2
Nandani Paliwal <nandanipaliwal127@gmail.com>
2024-03-06 15:23:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+/* --------------------------- CSS File import---------------------------------- */
+
+import "../src/assets/style/website.css";
+
+/* ---------------------------Screens import----------------------------------- */
+import Website from "../src/assets/pages/Website";
+import Login from "../src/assets/pages/Login";
+import Signup from "../src/assets/pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Website />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
